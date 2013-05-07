c0cda21f44ab2097ec0a58b515da1be17995899f
Daniel Casimir Grigg <mail@danielgrigg.com>
2013-05-07 20:19:42 +1000

    Bump cause maven is stupid

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slitrace "0.1.2"
+(defproject slitrace "0.1.3"
   :description "A suite of ray-tracing tools."
   :url "http://sliplanesoftware.com"
   :license {:name "Eclipse Public License"
