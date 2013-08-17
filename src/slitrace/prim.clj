2ba5a80ad0026b7b499aa21f3f61b30377963efd
Daniel Grigg <mail@danielgrigg.com>
2013-08-17 15:13:23 +1000

    Add grid tracing

--- src/slitrace/prim.clj
+++ src/slitrace/prim.clj
@@ -24,9 +24,9 @@
 
 (defn trace-list "trace a primitive seq" [ray primitives]
   (let [trace-sorted (fn [[t _ _ :as closest-geometry] prim ]
-                       (or (trace prim (ray-interval ray t)) closest-geometry))]
-    (reduce trace-sorted [(:maxt ray) nil nil] primitives)))
-
+                       (or (trace prim (ray-interval ray t)) closest-geometry))
+        closest (reduce trace-sorted [(:maxt ray) nil nil] primitives)]    
+    (if (second closest) closest)))
 
 
 (deftype ListGroup [^Transform transformation ^BBox bounds primitives]
@@ -34,8 +34,8 @@
   (trace [this _r]
     (let [^Ray r _r
           ^Ray r-local (transform r (inverse transformation))
-          [tn pn nn] (trace-list r-local primitives)]
-      (if pn
+          [tn pn nn :as geometry] (trace-list r-local primitives)]
+      (if geometry
         [tn (transform-point transformation pn) 
          (transform-normal transformation nn)]))))
 
@@ -54,9 +54,24 @@
   (trace [this _r]
          nil
          ))
-(comment 
+(comment (do 
   (use '[slitrace core shape prim]) 
   (use '[sligeom core bounding transform intersect aggregate])
-  (defn test-grid [] (grid (bbox (point3 0 0 0) (point3 9 9 9)) 1))
-  (map #(voxel-idx G)))
+  (defn test-grid [] (grid (bbox (point3 0 0 0) (point3 9 9 9)) 12))
+  (defn point-grid-seq [g r] (map #(voxel-to-point % g)(grid-seq g r)))
 
+  (def G (grid-add (sphere 4.0) (test-grid)))
+  (def G2 (grid-add (instance (translate 2 0 0) (sphere 2.0)) (test-grid)))
+  (def r2 (ray (point3 9 1 1) (vector3 -1 0 0)))
+  (->> (grid-seq G2 r2)
+	   (map #(voxel-idx % G2))
+	   (map (:voxels G2))
+	   (map #(trace-list r2 %))
+           (some identity))))
+
+(defn trace-grid [g r]
+  (->> (grid-seq g r)
+	   (map #(voxel-idx % g))
+	   (map (:voxels g))
+	   (map (partial trace-list r))
+           (some identity)))
